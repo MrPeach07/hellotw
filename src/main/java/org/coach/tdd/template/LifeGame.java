bd06d0676859f22efef9d707e9088f8f492b941c
MrPeach <767130703@qq.com>
2017-06-03 13:42:54 +0800

    commit6： 构建检测地图中四个角落的测试案例 实现检测四个角落代码

--- src/main/java/org/coach/tdd/template/LifeGame.java
+++ src/main/java/org/coach/tdd/template/LifeGame.java
@@ -1,11 +1,18 @@
 package org.coach.tdd.template;
 
+import java.util.List;
+
 /**
  * Created by lzy on 17-6-3.
  */
 public class LifeGame {
-	private int framesize = 20; //框架大小，表示为正方体框架的边长
-	private int[][] frame = new int[framesize][framesize];
+	private int framesize=0; //框架大小，表示为正方体框架的边长
+	private int[][] frame ;
+    private List<Cell> lifeCells;
+
+	public LifeGame(int framesize) {
+		this.framesize = framesize;
+	}
 
 	public int getFramesize() {
 		return framesize;
@@ -21,5 +28,71 @@ public class LifeGame {
 
 	public void setFrame(int[][] frame) {
 		this.frame = frame;
+	}
+	public void init(){
+		frame=new int[framesize][framesize];
+	}
+	public void putCell(Cell cell){
+		frame[cell.getLocation_x()][cell.getLocation_y()]=cell.getCellStatus();
+
+	}
+	public int[][] getAroundStatus(int x,int y){
+		BasicUnit basicUnit = new BasicUnit();
+		if(0==x&&0==y){
+			int [][] aroundUnit={{frame[x][y+1],frame[x+1][y],frame[x+1][y+1]},{0,0,0},{0,0,0}};
+			return aroundUnit;
+
+		}
+		else if(0==x&&framesize-1==y){
+			int [][] aroundUnit={{frame[x-1][y],frame[x+1][y-1],frame[x+1][y]},{0,0,0},{0,0,0}};
+			return aroundUnit;
+		}
+		else if(framesize-1==x&&0==y){
+			int [][] aroundUnit={{frame[x-1][y],frame[x-1][y+1],frame[x][y+1]},{0,0,0},{0,0,0}};
+			return aroundUnit;
+			//			return {{frame[x][y+1],frame[x+1][y],frame[x+1][y+1]},{0,0,0}{0,0,0}};
+		}
+		else if(framesize-1==x&&framesize-1==y){
+			int [][] aroundUnit={{frame[x-1][y],frame[x-1][y-1],frame[x][y-1]},{0,0,0},{0,0,0}};
+			return aroundUnit;
+		}
+		else if(0==x&&0!=y&&framesize-1!=y){
+			int [][] aroundUnit={{frame[x][y-1],frame[x][y+1],frame[x+1][y-1]},{frame[x+1][y],0,frame[x+1][y+1]},{0,0,0}};
+			return aroundUnit;
+		}
+
+		else if (framesize-1==x&&0!=y&&framesize-1!=y){
+			int [][] aroundUnit= {{frame[x-1][y-1],frame[x-1][y],frame[x-1][y+1]},{frame[x][y-1],0,frame[x][y+1]},{0,0,0}};
+			return  aroundUnit;
+		}
+		else if (0==y&&0!=x&&framesize-1!=x){
+			int [][] aroundUnit= {{frame[x-1][y],frame[x-1][y+1],frame[x][y+1]},{frame[x+1][y],0,frame[x+1][y+1]},{0,0,0}};
+			return  aroundUnit;
+		}
+		else if (framesize-1==y&&0!=x&&framesize-1!=x){
+			int [][] aroundUnit= {{frame[x-1][y-1],frame[x-1][y],frame[x][y-1]},{frame[x+1][y-1],0,frame[x+1][y]},{0,0,0}};
+            return aroundUnit;
+		}
+		else {
+			int [][] aroundUnit= {{frame[x-1][y-1],frame[x-1][y],frame[x-1][y]},{frame[x][y-1],0,frame[x][y+1]},{frame[x+1][y-1],frame[x+1][y],frame[x+1][y+1]}};
+			return  aroundUnit;
+
+		}
+	}
+	public void freshFrame(){
+		BasicUnit basicUnit=new BasicUnit();
+		for(int i=0;i<framesize;i++){
+			for (int j =0;j<framesize;j++){
+				int[][] aroundUnit = getAroundStatus(i,j);
+				int status=basicUnit.getUnitCelltatus(aroundUnit);
+				frame[i][j]=status;
+			}
+		}
+		for(int i=0;i<framesize;i++) {
+			for (int j = 0; j < framesize; j++) {
+				System.out.print(frame[i][j]);
+
+			}
+		}
 	}
 }
--- src/test/java/org/coach/tdd/template/LibraryTest.java
+++ src/test/java/org/coach/tdd/template/LibraryTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertThat;
 public class LibraryTest {
     @Test
     public void when_three_cells_around_is_live() {
-        LifeGame lifeGame =new LifeGame();
         BasicUnit basicUnit = new BasicUnit();
         int threelive[][] = {{0,0,0},{0,0,1},{1,1,0}};
 //        basicUnit.setTestUnit(threelive);
@@ -17,7 +16,6 @@ public class LibraryTest {
 
     @Test
     public void when_two_cells_around_is_keep() {
-        LifeGame lifeGame = new LifeGame();
         BasicUnit basicUnit = new BasicUnit();
         int twolive[][] = {{0,0,0},{0,0,1},{0,0,1}};
         assertThat(basicUnit.getUnitCelltatus(twolive),is(0));
@@ -25,7 +23,6 @@ public class LibraryTest {
 
     @Test
     public void when_other_numberofcells_around_is_death() {
-        LifeGame lifeGame = new LifeGame();
         BasicUnit basicUnit = new BasicUnit();
         int otherNumber1[][]= {{0,0,0},{0,0,1},{0,0,0}};
         int otherNumber4[][]= {{0,0,1},{0,1,1},{1,0,1}};
@@ -34,4 +31,27 @@ public class LibraryTest {
         assertThat(basicUnit.getUnitCelltatus(otherNumber4),is(0));
         assertThat(basicUnit.getUnitCelltatus(otherNumber5),is(0));
     }
+
+    @Test
+	public void get_lifegame_cell_around_of_4_corner_msg(){
+    	LifeGame lifeGame = new LifeGame(4);
+    	lifeGame.init();
+    	lifeGame.getAroundStatus(0,0);
+    	int[][] result = {{0,0,0},{0,0,0},{0,0,0}};
+//    	assertArrayEquals(lifeGame.getAroundStatus(0,0),result);
+		assertThat(lifeGame.getAroundStatus(0,0),is(result));
+	}
+    @Test
+	public void lifegame_run_init_1_cell_live() {
+    	LifeGame lifeGame=new LifeGame(4);
+    	lifeGame.init();
+    	Cell cell1 =new Cell(1,2,1);
+//    	Cell cell2 =new Cell(2,2,1);
+//    	Cell cell3 =new Cell(2,3,1);
+    	lifeGame.putCell(cell1);
+//    	lifeGame.init(cell2);
+//    	lifeGame.init(cell3);
+		lifeGame.freshFrame();
+
+	}
 }
