f3b6290f398f8c74a6a018bf5653d1cc457f898c
MrPeach <767130703@qq.com>
2017-06-03 11:30:54 +0800

    commit4： 完成测试案例3：周围除2个细胞或者3个细胞生存时，返回原细胞的生命状态为死亡 测试案例代码和实现代码代码实现

--- src/test/java/org/coach/tdd/template/LibraryTest.java
+++ src/test/java/org/coach/tdd/template/LibraryTest.java
@@ -14,6 +14,7 @@ public class LibraryTest {
 //        basicUnit.setTestUnit(threelive);
         assertThat(basicUnit.getUnitCelltatus(threelive),is(1));
     }
+
     @Test
     public void when_two_cells_around_is_keep() {
         LifeGame lifeGame = new LifeGame();
@@ -21,4 +22,17 @@ public class LibraryTest {
         int twolive[][] = {{0,0,0},{0,0,1},{0,0,1}};
         assertThat(basicUnit.getUnitCelltatus(twolive),is(0));
     }
+
+    @Test
+    public void when_other_numberofcells_around_is_death() {
+        LifeGame lifeGame = new LifeGame();
+        BasicUnit basicUnit = new BasicUnit();
+        int otherNumber1[][]= {{0,0,0},{0,0,1},{0,0,0}};
+        int otherNumber4[][]= {{0,0,1},{0,1,1},{1,0,1}};
+        int otherNumber5[][]= {{1,0,1},{0,1,1},{1,0,1}};
+        assertThat(basicUnit.getUnitCelltatus(otherNumber1),is(0));
+        assertThat(basicUnit.getUnitCelltatus(otherNumber4),is(0));
+        assertThat(basicUnit.getUnitCelltatus(otherNumber5),is(0));
+    }
+
 }
